df5bfafb7e32f06140055c15f731cc0271887001
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-21 22:00:58 +0100

    adds dashboard composition

---
+++ src/routes/dashboard.tsx
@@ -0,0 +1,27 @@
+import { lazy, Suspense } from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+
+import Layout from "../components/layout";
+import Loading from "../components/loading";
+
+const Dashboard = lazy(() => import("../pages/dashboard"));
+
+const App = () => {
+  const { path } = useRouteMatch();
+
+  console.log(path, "hello");
+
+  return (
+    <Layout>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path={`${path}`}>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
+  );
+};
+
+export default App;
